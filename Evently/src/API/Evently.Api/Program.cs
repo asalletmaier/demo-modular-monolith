8e9c963b8f818d2e5d740dd2501062108b2edbec
asalletmaier <a.salletmaier@outlook.at>
2025-04-29 20:45:14 +0200

    First modules; Docker Setup

---
+++ Evently/src/API/Evently.Api/Program.cs
@@ -0,0 +1,23 @@
+using Evently.Api.Extensions;
+using Evently.Modules.Events.Api;
+using Scalar.AspNetCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddEventsModule(builder.Configuration);
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+
+    app.ApplyMigrations();
+}
+
+EventsModule.MapEndpoints(app);
+
+app.Run();
---
+++ Evently/src/Modules/Events/Evently.Modules.Events.Api/Events/GetEvent.cs
@@ -0,0 +1,30 @@
+using Evently.Modules.Events.Api.Database;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evently.Modules.Events.Api.Events;
+
+public static class GetEvent
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("events/{id}", async (Guid id, EventsDbContext context) =>
+        {
+            EventResponse @event = await context.Events
+                .Where(x => x.Id == id)
+                .Select(x => new EventResponse(
+                    x.Id,
+                    x.Title,
+                    x.Description,
+                    x.Location,
+                    x.StartsAtUtc,
+                    x.EndsAtUtc))
+                .SingleOrDefaultAsync();
+
+            return @event is null ? Results.NotFound() : Results.Ok(@event);
+        })
+        .WithTags(Tags.Events);
+    }
+}
